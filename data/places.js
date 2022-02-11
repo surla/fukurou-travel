542153ddce8f469d6dea43c5d0b7636e1bb18d55
Joseph Surla <josephsurla@gmail.com>
2022-02-10 19:11:03 -0800

    add places data

---
+++ data/places.js
@@ -0,0 +1,36 @@
+export const places = [
+  {
+    id: "bc576a2c-eca5-4ccb-a494-c7b91d150564",
+    imageUrl: "",
+    title: "Kochia Hill",
+    location: "Hitachinaka, Japan",
+    description: "See the beautiful sea of crimson red kochia bushes.",
+    category: "Nature",
+  },
+  {
+    id: "fdef7068-85d2-4e14-a0d9-58e2b7b7b674",
+    imageUrl: "",
+    title: "Kawachi Wisteria Tunnel",
+    location: "Kitayushu, Japan",
+    description: "Walk through a bautiful tunnel of cascading violet flowers.",
+    category: "Nature",
+  },
+  {
+    id: "1405e2d8-faf1-4dd6-a35f-26d80aed7dbd",
+    imageUrl: "",
+    title: "Giant Gundam Robot",
+    location: "Tokyo, Japan",
+    description:
+      "A giant white statue of a cult sci-fi novel & anime character.",
+    category: "Attraction",
+  },
+  {
+    id: "dad1aaeb-d727-4160-847a-52ef7b10e51b",
+    imageUrl: "",
+    title: "Ghibli Museum",
+    location: "Tokyo, Japan",
+    description:
+      "Explore the whimsical museum dedicated to the famed animation studio.",
+    category: "Museum",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { places } from "../data/places";
 
 export default function Home() {
   return (
@@ -13,7 +14,21 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="text-center text-3xl font-bold">fukurou.</h1>
+        <h1 className="text-center text-3xl font-bold mt-5">fukurou.</h1>
+        <div className="container mx-auto my-20 max-w-5xl">
+          <h3 className="text-xl mb-5 font-bold">Explore</h3>
+          <ul className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+            {places.map((place) => (
+              <li key={place.id} className="max-w-md rounded shadow">
+                <div className="flex flex-col space-y-2 p-5">
+                  <p className="text-sm text-blue-500">{place.category}</p>
+                  <p className="text-lg font-medium">{place.title}</p>
+                  <p className="text-gray-600">{place.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </main>
     </div>
   );
